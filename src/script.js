fbe07cbc461a73b21457852798d92049b77e8bd2
Mohammed Abdul Aziz <cloudaziz@gmail.com>
2023-08-09 18:44:21 +0600

    adding cart page & styling on  process

--- src/script.js
+++ src/script.js
@@ -1,29 +1,5 @@
 let shop = document.querySelector('#shop');
 
-let shopData = [
-  {
-    id: 1,
-    name: 'Casual Shoe',
-    desc: 'Product Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ab?',
-    price: 32,
-    img: './images/hero.png',
-  },
-  {
-    id: 2,
-    name: 'Perty Shoe',
-    desc: 'Product Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ab?',
-    price: 44,
-    img: './images/hero2.png',
-  },
-  {
-    id: 3,
-    name: 'Perty Beg',
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam animi officia voluptate dicta inventore corrupti?',
-    price: 55,
-    img: './images/hero3.png',
-  },
-];
-
 let basket = JSON.parse(localStorage.getItem('basketdata')) || [];
 
 let generateShopItem = () => {
